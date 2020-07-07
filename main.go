b76b392f0874b37034f837c21df37b3b24d2ea38
yaozongyi <yaozongyi1986@gmail.com>
2020-07-07 23:17:58 +0900

    簡単なログイン機能を追加する

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,8 +12,18 @@ func main() {
 	//router.LoadHTMLFiles("templates/template1.html", "templates/template2.html")
 	router.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
+			"title": "ログイン画面",
 		})
 	})
+	var username string
+	var password string
+	router.POST("/login", func(context *gin.Context) {
+		username = context.PostForm("username")
+		password = context.PostForm("password")
+		fmt.Print(username)
+		fmt.Println(password)
+		context.HTML(http.StatusOK, "loginSuccess.html", gin.H{"title": "ログイン成功画面", "status": "ログイン成功"})
+	})
+
 	router.Run(":8080")
 }
